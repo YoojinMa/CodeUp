b087e6c88b6c6dd690799cf5232d098e56a88feb
YoojinMa <geronimo9595@naver.com>
2021-11-01 23:55:24 +0900

    [Solved] 6044

---
+++ 6044.py
@@ -0,0 +1,14 @@
+'''
+문제 - 기초-산술연산
+코드업 6044 정수 2개 입력받아 자동 계산하기
+https://codeup.kr/problem.php?id=6044
+'''
+
+
+a, b = map(int, input().split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+print(format(a / b, ".2f"))
