5535a4202ebec1f57d400d8081fe5ec9ba7bec70
YoojinMa <geronimo9595@naver.com>
2021-11-27 23:53:50 +0900

    [Solved] 6066

---
+++ 6066.py
@@ -0,0 +1,20 @@
+'''
+문제 - 기초-조건/선택실행구조
+코드업 6065 정수 3개 입력받아 짝/홀 출력하기
+https://codeup.kr/problem.php?id=6065
+'''
+
+
+a, b, c = map(int, input().split())
+if (a % 2 == 0):
+    print("even")
+else:
+    print("odd")
+if (b % 2 == 0):
+    print("even")
+else:
+    print("odd")
+if (c % 2 == 0):
+    print("even")
+else:
+    print("odd")
