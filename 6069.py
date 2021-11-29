691283e12b6535a917c0d830b9aba74084c7eee4
YoojinMa <geronimo9595@naver.com>
2021-11-30 00:12:05 +0900

    [Solved] 6069

---
+++ 6069.py
@@ -0,0 +1,18 @@
+'''
+문제 - 기초-조건/선택실행구조
+코드업 6069 평가 입력받아 다르게 출력하기
+https://codeup.kr/problem.php?id=6069
+'''
+
+
+a = input()
+if (a == "A"):
+    print("best!!!")
+elif (a == "B"):
+    print("good!!")
+elif (a == "C"):
+    print("run!")
+elif (a == "D"):
+    print("slowly~")
+else:
+    print("what?")
