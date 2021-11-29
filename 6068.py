8a9b7e35354f001b03f2a8c86fe820d3e60880ff
YoojinMa <geronimo9595@naver.com>
2021-11-29 23:52:17 +0900

    [Solved] 6068

---
+++ 6068.py
@@ -0,0 +1,16 @@
+'''
+문제 - 기초-조건/선택실행구조
+코드업 6068 점수 입력받아 평가 출력하기
+https://codeup.kr/problem.php?id=6068
+'''
+
+
+a = int(input())
+if (a >= 90):
+    print("A")
+elif (a >= 70):
+    print("B")
+elif (a >= 40):
+    print("C")
+else:
+    print("D")
