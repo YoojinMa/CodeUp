786d844f3ac2e51c862eba44c72c03d5d47bf4cb
YoojinMa <geronimo9595@naver.com>
2021-12-05 20:20:01 +0900

    [Solved] 6071

---
+++ 6071.py
@@ -0,0 +1,14 @@
+'''
+문제 - 기초-반복실행구조
+코드업 6071 0 입력될 때까지 무한 출력하기
+https://codeup.kr/problem.php?id=6071
+'''
+
+
+a = 1
+while (a != 0):
+    a = int(input())
+    if (a != 0):
+        print(a)
+    else:
+        break
