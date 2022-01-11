7ad2ad1e773fcd08a200a78cf73ddfbf4a6a7620
YoojinMa <geronimo9595@naver.com>
2022-01-11 10:06:55 +0900

    [Solved] 6084

---
+++ 6084.py
@@ -0,0 +1,12 @@
+'''
+문제 - 기초-종합
+코드업 6083 소리 파일 저장용량 계산하기
+https://codeup.kr/problem.php?id=6084
+'''
+
+
+h, b, c, s = map(int, input().split())
+
+result = h * b * c * s / 8 / 1024 / 1024
+
+print(format(result, ".1f") + " MB")
